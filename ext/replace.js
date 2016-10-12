fc4e9b4311b092acea96d12e2fd0aea4c5e195c1
Andrew Carter <andrewcarter1992@gmail.com>
2016-10-12 13:24:33 +0100

    Gave timeout to replace check

--- ext/replace.js
+++ ext/replace.js
@@ -1,10 +1,12 @@
+var tick = 100;
+
 function try_replace()
 {
 	var p = document.getElementById('loading_welcome_msg');
 
 	if (p !== null) {
 		p.innerHTML = 'Go fuck yourself';
-	} else {
+	} else if (tick-- > 0) {
 		setTimeout(try_replace, 50);
 	}
 }
